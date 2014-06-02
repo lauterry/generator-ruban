d8f7f6965682d26a5f7e0a5854b634b0c06b866d
LAU Thierry <thierlau@gmail.com>
2014-06-02 03:41:35 +0200

    add prompts

--- app/index.js
+++ app/index.js
@@ -7,48 +7,59 @@ var chalk = require('chalk');
 
 
 var RubanGenerator = yeoman.generators.Base.extend({
-  init: function () {
-    this.pkg = require('../package.json');
+	init: function () {
+		this.pkg = require('../package.json');
 
-    this.on('end', function () {
-      if (!this.options['skip-install']) {
-        this.installDependencies();
-      }
-    });
-  },
+		this.on('end', function () {
+			if (!this.options['skip-install']) {
+				this.installDependencies();
+			}
+		});
+	},
 
-  askFor: function () {
-    var done = this.async();
+	askFor: function () {
+		var done = this.async();
 
-    // Have Yeoman greet the user.
-    this.log(yosay('Welcome to the marvelous Ruban generator!'));
+		// Have Yeoman greet the user.
+		this.log(yosay('Welcome to the marvelous Ruban generator!'));
 
-    var prompts = [{
-      type: 'confirm',
-      name: 'someOption',
-      message: 'Would you like to enable this option?',
-      default: true
-    }];
+		var prompts = [
+			{
+				type: 'input',
+				name: 'title',
+				message: 'What is the title of your prez ?',
+				default: 'A great prez'
+			},
+			{
+				type: 'input',
+				name: 'author',
+				message: "What is your name ?"
+			},
+			{
+				type: 'input',
+				name: 'twitter',
+				message: "What is your twitter account ?"
+			}
+		];
 
-    this.prompt(prompts, function (props) {
-      this.someOption = props.someOption;
+		this.prompt(prompts, function (props) {
+			console.log(JSON.stringify(props));
+			done();
+		}.bind(this));
+	},
 
-      done();
-    }.bind(this));
-  },
+	app: function () {
+		this.mkdir('app');
+		this.mkdir('app/templates');
 
-  app: function () {
-    this.mkdir('app');
-    this.mkdir('app/templates');
+		this.copy('_package.json', 'package.json');
+		this.copy('_bower.json', 'bower.json');
+	},
 
-    this.copy('_package.json', 'package.json');
-    this.copy('_bower.json', 'bower.json');
-  },
-
-  projectfiles: function () {
-    this.copy('editorconfig', '.editorconfig');
-    this.copy('jshintrc', '.jshintrc');
-  }
+	projectfiles: function () {
+		this.copy('editorconfig', '.editorconfig');
+		this.copy('jshintrc', '.jshintrc');
+	}
 });
 
 module.exports = RubanGenerator;
