166fdb0623d3e9bb27569fa79307232ce75a6b24
LAU Thierry <thierlau@gmail.com>
2014-06-06 09:18:41 +0200

    add app.js

---
+++ app/templates/app.js
@@ -0,0 +1,11 @@
+$( document ).ready(function() {
+	new Ruban({
+		"pagination": true,
+		"ratio": 1.3333333333333333,
+		"minPadding": "1em",
+		"transitionDuration": "0.8s",
+		"stripHtmlInToc": false,
+		"bindClicks": false,
+		"bindMouseWheel": false
+	});
+});
